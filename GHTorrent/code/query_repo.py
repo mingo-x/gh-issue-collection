4f92e0beede0cfb2edb2f188d2350951e626cca1
Yanping <rea.s.xyp@gmail.com>
2017-09-14 23:14:56 +0200

    add break-point for repos

--- GHTorrent/code/query_repo.py
+++ GHTorrent/code/query_repo.py
@@ -2,8 +2,20 @@ from pymongo import MongoClient
 import pprint
 import time
 from bson.objectid import ObjectId
+import sys
 
-fout = open("./data/repos_4.out",'w')
+skip_num = 0 #2583280 for repo5, 2404314 for repo4, 2120741 for repo3, 1877449 for repo2, 367376 for repo1, 0 for repo0
+out_index = "x"
+if len(sys.argv)<5:
+	print("please specify the number of records to skip by \"-s xxx\" and the out file index by \"-i xxx\"")
+	exit()
+for i in range(1,len(sys.argv),2):
+	if sys.argv[i] == '-s':
+		skip_num = int(sys.argv[i+1])
+	elif sys.argv[i] == '-i':
+		out_index = sys.argv[i+1]
+
+fout = open("./data/repos_"+out_index+".out",'w')
 client = MongoClient('mongodb://127.0.0.1:27017/',unicode_decode_error_handler='ignore')
 db = client.github
 #print("test ",db.issues.count())
@@ -11,7 +23,7 @@ db = client.github
 #64631770
 c = 0
 startTime = time.time()
-for repo in db.repos.find().skip(2404310):
+for repo in db.repos.find().skip(skip_num):
 	# pprint.pprint(repo)
 	fout.write("{\"created_at\":\""+repo['created_at']+"\",\"name\":\""+repo['name']+"\",\"owner\":\""+repo['owner']['login']+"\"}\n")
 	c = c+1

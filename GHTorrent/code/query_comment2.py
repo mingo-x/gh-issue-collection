e2e37a544a1d9e4dd5ff9603815af0924d61f778
Yanping <rea.s.xyp@gmail.com>
2017-10-05 23:14:23 +0200

    add skip

--- GHTorrent/code/query_comment2.py
+++ GHTorrent/code/query_comment2.py
@@ -8,12 +8,16 @@ import helper
 # settings
 dir_path = "/mnt/ds3lab/yanping/"
 idx = "0"
+skip_num = 0
 for i in range(1,len(sys.argv),2):
 	if sys.argv[i]=="-i":
 		idx = sys.argv[i+1]
+		print("idx:",idx)
+	elif sys.argv[i]=="-s":
+		skip_num = int(sys.argv[i+1])
 fin = open(dir_path+"data/issues_"+idx+".out","r")
-fout = open(dir_path+"data/comments_"+idx+".out",'w',encoding='utf-8')
-flog = open(dir_path+"log/comments_"+idx+".out",'w',encoding='utf-8')
+fout = open(dir_path+"data/comments_"+idx+".out",'a',encoding='utf-8')
+flog = open(dir_path+"log/comments_"+idx+".out",'a',encoding='utf-8')
 
 client = MongoClient('mongodb://127.0.0.1:27017/',unicode_decode_error_handler='ignore')
 db = client.github
@@ -21,8 +25,10 @@ db = client.github
 issue_count = 0
 start_time = time.time()
 for line in fin:
-	issue = json.loads(line)
 	issue_count = issue_count+1
+	if issue_count <= skip_num:
+		continue
+	issue = json.loads(line)
 	comments = db.issue_comments.find({"repo":issue['repo'],"owner":issue['repo_owner'],"issue_id":issue['number']}).sort('created_at',ASCENDING)
 	comment_buffer = []
 	users = set()

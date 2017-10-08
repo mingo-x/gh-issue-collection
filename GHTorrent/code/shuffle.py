14ddd49ed395b4009a582665007953d2ba2e6bab
Yanping <rea.s.xyp@gmail.com>
2017-10-08 16:54:14 +0200

    shuffle

--- GHTorrent/code/shuffle.py
+++ GHTorrent/code/shuffle.py
@@ -1,28 +1,25 @@
 import json
+import random
 
 dir_path = "/mnt/ds3lab/yanping/data/"
-out_idx = 0
-counter = 0
-unit = 100000
-fout = open(dir_path+"comments_shuffled_"+str(out_idx)+".out",'w',encoding='utf-8')
+fout = [open(dir_path+"comments_shuffled_"+str(out_idx)+".out",'w',encoding='utf-8') for out_idx in range(12)]
 #4 to 22
 for idx in range(4,23):
+	counter = 0
 	fin = open(dir_path+"comments_"+str(idx)+".out",'r')
 	line = fin.readline()
 	while line:
-		if counter == unit:
-			out_idx = out_idx+1
-			fout.close()
-			fout = open(dir_path+"comments_shuffled_"+str(out_idx)+".out",'w',encoding='utf-8')
-			counter = 0
-		counter = counter+1
+		out_idx = random.randint(0,11)
+		counter = counter + 1
 		if counter%10000 == 0:
-			print(idx,out_idx,counter)
-		fout.write(line)
+			print(idx,counter,flush=True)
+		fout[out_idx].write(line)
 		issue = json.loads(line)
 		for i in range(issue['comments']):
 			line = fin.readline()
-			fout.write(line)
+			fout[out_idx].write(line)
 		line = fin.readline()
 	fin.close()
-fout.close()
+
+for i in range(12):
+	fout[i].close()

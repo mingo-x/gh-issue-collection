7332695b2ffd55bc73ef4e7367c2aec0905381a1
Yanping <rea.s.xyp@gmail.com>
2017-10-08 17:00:25 +0200

    start/end option

--- GHTorrent/code/shuffle.py
+++ GHTorrent/code/shuffle.py
@@ -1,10 +1,20 @@
 import json
 import random
+import sys
 
+start = 4
+end = 22
+for i in range(1,len(sys.argv)):
+	if sys.argv[i] == "-a":
+		start = int(sys.argv[i+1])
+		print("start from file",start)
+	elif sys.argv[i] == "-b":
+		end = int(sys.argv[i+1])
+		print("end by file", end)
 dir_path = "/mnt/ds3lab/yanping/data/"
 fout = [open(dir_path+"comments_shuffled_"+str(out_idx)+".out",'w',encoding='utf-8') for out_idx in range(12)]
 #4 to 22
-for idx in range(4,23):
+for idx in range(start,end+1):
 	counter = 0
 	fin = open(dir_path+"comments_"+str(idx)+".out",'r')
 	line = fin.readline()

f0e133f85118c6f0eee4fe4092b11bebb7d5f66b
Yanping <rea.s.xyp@gmail.com>
2017-11-17 21:09:09 +0100

    .

--- GHTorrent/code/annotator.py
+++ GHTorrent/code/annotator.py
@@ -2,47 +2,51 @@ import json
 import sys
 import os
 
-dir_name = "/mnt/ds3lab/yanping"
-counter = 0
-i = 0
-append = False
-fin = open(dir_name+"/data/comments_batch_"+str(i)+".out","r",encoding='utf-8')
-fout = open(dir_name+"/data/annotation/"+str(i)+".txt","w",encoding='utf-8')
-line = fin.readline()
+def annotate():
+	dir_name = "/mnt/ds3lab/yanping"
+	counter = 0
+	i = 0
+	append = False
+	fin = open(dir_name+"/data/comments_batch_"+str(i)+".out","r",encoding='utf-8')
+	fout = open(dir_name+"/data/annotation/"+str(i)+".txt","w",encoding='utf-8')
+	line = fin.readline()
 
-while line:
-	issue = json.loads(line)
-	if 'l' in issue:
-		if not append:
-			fout.write(line)
+	while line:
+		issue = json.loads(line)
+		if 'l' in issue:
+			if not append:
+				fout.write(line)
+				for j in range(issue['comments']):
+					line = fin.readline()
+					fout.write(line)
+			fout.flush()
+		else:
+			counter += 1
+			print("no.",counter)
+			print(line)
+			comments = []
 			for j in range(issue['comments']):
 				line = fin.readline()
-				fout.write(line)
-		fout.flush()
-	else:
-		counter += 1
-		print("no.",counter)
-		print(line)
-		comments = []
-		for j in range(issue['comments']):
-			line = fin.readline()
-			comments += line
-			print(line)
-		label = input("label:")
-		while label<'0' or label>'9':
-			label = input("label 0~9:")
-		fout.write(line[:-2]+",\"l\":"+label+"}\n")
-		for j in range(issue['comments']):
-			fout.write(comments[j])
-		cont = input("continue?")
-		fout.flush()
-		if cont != "":
-			fin.close()
+				comments += line
+				print(line)
+			label = input("label:")
+			while label<'0' or label>'9':
+				label = input("label 0~9:")
+			fout.write(line[:-2]+",\"l\":"+label+"}\n")
+			for j in range(issue['comments']):
+				fout.write(comments[j])
+			cont = input("continue?")
 			fout.flush()
-			os.fsync(fout.fileno())
-			fout.close()
-			sys.exit()
-	line = fin.readline()
-fin.close()
-fout.close()
+			if cont != "":
+				fin.close()
+				fout.flush()
+				#os.fsync(fout.fileno())
+				fout.close()
+				return
+		line = fin.readline()
+	fin.close()
+	fout.close()
+
+if __name__ == "__main__":
+	annotate()
 
